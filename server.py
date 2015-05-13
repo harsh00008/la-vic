f2612fe39ad9b0affb3c9bc145b2da8d8cdf8c6f
Harsh Malewar <harsh00008@gmail.com>
2015-05-13 15:10:03 -0700

    Working code on multiple node

--- server.py
+++ server.py
@@ -4,155 +4,130 @@ import json
 import sys
 import socket
 from colorama import init, Fore, Back, Style
-import threading
-import thread
+from threading import Thread
 
 
 Pyro4.config.REQUIRE_EXPOSE = True
 
+order_json=None
+my_uri = ""
+
 class Server(object):
-    my_uri = ""
-    def __init__(self, name):
-        init()
-        self.clear_screen()
-        self.queue_address = "PYRONAME:lavic.queue.readyOrders"
-        self.print_main_title("NEW SERVER " + str(name).upper())
-        self.server_name = name
-        self.print_info("Hi, my name is " + str(self.server_name) +". I'm starting my work")
-        try:
-            self.print_network("Connecting to " + str(self.queue_address) + "...")
-            self.orders = Pyro4.Proxy(self.queue_address)
-            self.ns=Pyro4.locateNS()
-            self.print_success("Connected to " + str(self.queue_address))
-        except:
-            self.print_error("Some problem connecting to queue. Exiting...")
-            exit()
-        self.order_json = None
-        self.address = None
-
-    def run(self):
-        self.print_success("Time to work!")
-        server_name = self.server_name
-        while True:
-            try:
-                if int(self.orders.get_size()) > 0:
-                    parsed_json = self.orders.get_order()
-                    if parsed_json is not None:
-                        message_type = parsed_json['messageType']
-                        if message_type == "ORDER_COMPLETE":
-                            token_number = parsed_json['order']['tokenNumber']
-                            self.print_title("Announcing token "+ str(token_number))
-                            parsed_json['messageType'] = "ORDER_PREPARED"
-                            self.order_json = parsed_json
-                            self._broadcast_customers()
-                time.sleep(6)
-            except:
-                self.print_error("Some problem getting json")
-
     @Pyro4.expose
     def take_order(self):
-        self.print_title("Hey! Thanks. Please take your order!")
-        response_json = self.order_json
-        self.order_json = None
+        global order_json
+        response_json = order_json
+        order_json = None
         return response_json
 
-    def _broadcast_customers(self):
-        count = 0
-        customer_list = self.ns.list("lavic.customer.")
-        shout_limit = 3
+def serverCustomer():
+    print("Time to work!")
+    global order_json
+    while True:
         try:
-            for count in range(0, shout_limit):
-                if self.order_json is not None:
-                    token_number = str(self.order_json['order']['tokenNumber'])
-                    self.print_info("Token number " + token_number + " Please take your order - Announcement: " + str(count + 1))
-                    for customer in customer_list:
-                        pyro_address = customer_list[customer]
-                        self._print_network("Announcing tokens to " + str(customer) + " : " + pyro_address)
-                        try:
-                            customer_obj = Pyro4.Proxy(pyro_address)
-                            customer_obj.listen("server", self.address, self.order_json )
-                            self.print_success("Sent")
-                        except:
-                            self.print_error("Some problem connecting to customers :" + str(customer))
-                else:
-                    self.order_json = None
-                    return
-                time.sleep(1)
+            orders = Pyro4.Proxy("PYRONAME:lavic.queue.readyOrders")
+            if int(orders.get_size()) > 0:
+                parsed_json = orders.get_order()
+                if parsed_json is not None:
+                    message_type = parsed_json['messageType']
+                    if message_type == "ORDER_COMPLETE":
+                        print_info("I've got an order to announce")
+                        token_number = parsed_json['order']['tokenNumber']
+                        print_title("Announcing token "+ str(token_number))
+                        parsed_json['messageType'] = "ORDER_PREPARED"
+                        order_json = parsed_json
+                        broadcast_customers()
+            time.sleep(6)
         except:
-            print("Some Problem in broadcast")
-        finally:
-            self.print_info("No takers. Sorry, disposing order.")
-            return
+            print_error("Some problem getting json")
 
-    def set_my_pyro(self, address):
-        self.print_success("Setting my address: " + address)
-        self.address = address
+def broadcast_customers():
+        global my_uri
+        global order_json
 
-    def print_success(self, message):
-        print(Back.GREEN + Style.BRIGHT  +  Fore.BLACK + " SUCCESS " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+        ns=Pyro4.locateNS()
+        customer_list = ns.list("lavic.customer.")
+        print(str(customer_list))
+        shout_limit = 3
+        count = 0
+        for count in range(0, shout_limit):
+            if not order_json is None:
+                token_number = str(order_json['order']['tokenNumber'])
+                print_info("Token number " + token_number + " Please take your order - Announcement: " + str(count + 1))
+                for customer in customer_list:
+                    pyro_address = customer_list[customer]
+                    print_network("Announcing tokens to " + str(customer) + " : " + pyro_address)
+                    try:
+                        global my_uri
+                        customer_obj = Pyro4.Proxy(pyro_address)
+                        customer_obj.listen("server", my_uri, order_json )
+                        print_success("Sent")
+                    except:
+                        print_error("Some problem connecting to customers :" + str(customer))
+            else:
+                order_json = None
+                return
+            time.sleep(1)
+        print_info("No takers. Sorry, disposing order.")
 
-    def print_info(self, message):
-        print(Back.BLUE + Style.BRIGHT  +  Fore.WHITE + " INFO " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+def print_success( message):
+    print(Back.GREEN + Style.BRIGHT  +  Fore.BLACK + " SUCCESS " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-    def print_error(self, message):
-        print( Back.RED + Style.BRIGHT  +  Fore.WHITE + " ERROR " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+def print_info( message):
+    print(Back.BLUE + Style.BRIGHT  +  Fore.WHITE + " INFO " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-    def print_network(self, message):
-        print( Back.YELLOW + Style.BRIGHT  +  Fore.BLACK + " NETWORK " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+def print_error( message):
+    print( Back.RED + Style.BRIGHT  +  Fore.WHITE + " ERROR " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-    def print_title(self, message):
-        print(Fore.GREEN + Style.BRIGHT)
-        print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        print("                 " + str(message))
-        print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+def print_network( message):
+    print( Back.YELLOW + Style.BRIGHT  +  Fore.BLACK + " NETWORK " + Fore.RESET + Back.RESET + Style.RESET_ALL +  Fore.WHITE + Style.BRIGHT + " : " + str(message))
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-    def print_main_title(self, message):
-        print(Fore.CYAN + Back.WHITE + Style.BRIGHT)
-        print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =")
-        print(Back.RESET)
-        print( "                        " + str(message).upper()+ "                     ")
-        print(Back.WHITE)
-        print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =")
-        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+def print_title(message):
+    print(Fore.GREEN + Style.BRIGHT)
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("                 " + str(message))
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-    def clear_screen(self):
-        print("\033c")
+def print_main_title(message):
+    print(Fore.CYAN + Back.WHITE + Style.BRIGHT)
+    print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =")
+    print(Back.RESET)
+    print( "                        " + str(message).upper()+ "                     ")
+    print(Back.WHITE)
+    print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =")
+    print(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-class ActivateThread (threading.Thread):
-    def __init__(self, server):
-        threading.Thread.__init__(self)
-        self.server = server
-    def run(self):
-        activate(self.server)
+def clear_screen():
+    print("\033c")
 
-def activate(server):
+
+def activate():
+    serverName = str(sys.argv[1])
+    server = Server()
     myIp = str(socket.gethostbyname(socket.gethostname()))
-    daemon=Pyro4.Daemon(myIp)                 # make a Pyro daemon
+    daemon=Pyro4.Daemon(myIp)           
     ns=Pyro4.locateNS()
     uri=daemon.register(server)
-    server.set_my_pyro(str(uri))
+    global my_uri
+    my_uri = uri
     daemon.requestLoop()
 
 if __name__ == "__main__":
     try:
-        serverName = str(sys.argv[1])
+        
+        server_activate = Thread(target=activate)
+        server_start = Thread(target=serverCustomer)
+        server_start.start()
+        server_activate.start()
+        server_activate.join()
+        server_start.join()
+
     except:
         print(Back.RED + Style.BRIGHT + Fore.WHITE  + " ERROR " + Fore.RESET + Back.RESET + Style.RESET_ALL + Fore.MAGENTA + Style.BRIGHT + " : Please specify a server name as an argument. Exiting...")
         print(Fore.RESET + Back.RESET + Style.RESET_ALL)
         exit()
-
-    server = Server(serverName)
-    activateThread = ActivateThread(server)
-    activateThread.start()
-    server.print_success("Running server now")
-    server.run()
-
-
-
-
-
