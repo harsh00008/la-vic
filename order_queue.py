a7ddfc026d15bb7f156375218a50abad221f0083
Harsh Malewar <harsh00008@gmail.com>
2015-05-14 17:32:51 -0700

    Updated order_queue

--- order_queue.py
+++ order_queue.py
@@ -8,36 +8,26 @@ import socket
 Pyro4.config.REQUIRE_EXPOSE = True
 
 ordersQueue = Queue.Queue()
-
 class OrderQueue(object):
-
-    def __init__(self):
-        print("Queue started")
 
     @Pyro4.expose
     def get_order(self):
-        if not ordersQueue.empty():
-            return ordersQueue.get()
+        return ordersQueue.get()
 
     @Pyro4.expose
     def add_order(self, json_message):
-        print("New order received!")
+        print("Order Ready : "+json_message['order']['tokenNumber'])
         ordersQueue.put(json_message)
 
+    @Pyro4.expose
+    def get_size(self):
+        return ordersQueue.qsize()
 
 order = OrderQueue()
 myIp = str(socket.gethostbyname(socket.gethostname()))
-print("My IP Address: " + myIp)
-daemon=Pyro4.Daemon(myIp)                 # make a Pyro daemon
+daemon=Pyro4.Daemon(myIp)
 ns=Pyro4.locateNS()
-uri=daemon.register(order)   		  # register the greeting object as a Pyro object
-ns.register("lavic.server", uri)
-print("Ready. uri =" + str(uri))      # print the uri so we can use it in the client later
-daemon.requestLoop()                  # start the event loop of the server to wait for calls
-
-
-
-
-
-
-
+uri=daemon.register(order)
+ns.register("lavic.queue.readyOrders", uri)
+print("Order Queue started on : " + str(uri))
+daemon.requestLoop()
